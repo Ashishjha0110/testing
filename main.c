1a755222f9c6286c0aca7ed8ac13adb7898315ac
Ashish Jha <ashishjhaeee@gmail.com>
2021-06-24 04:31:56 +0000

    Sum of first n natural number.

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
-/*program to print multiplication table*/
+/*program to sum first n numbers in O(n) then O(1) */
 
 #include<stdio.h>
 
 int main()
 {
-  int num,result;
+  int num,sum=0;
   
-  printf("Enter the nuumber:");
+  printf("Enter the last natural nuumber:");
   scanf("%d",&num);
+ //sum= 1+2+3+.............n
+ for(int i=1;i<=num;i++)
+ {
+   sum= (num*num+num)/2;//time complexity O(1)
   
-  for(int i=1;i<=10;i++)
-  {
-    result=num*i;
-    printf("%dx%d=%d\n",num,i,result);
-  }
-  
-  
+ }
+  printf("The sum of %d natural number:%d\n",num,sum);  
   return 0;
 }
