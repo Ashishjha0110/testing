4779a6a10f2380354fde4fd9cd460c25b1df29a0
Ashish Jha <ashishjhaeee@gmail.com>
2021-07-23 16:12:58 +0000

    triangle pattern

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
-/*program to convert decimal to binary number*/
+/*program to print *
+                  ***
+                 ***** */
 
 #include<stdio.h>
 
-
 int main()
 {
-  int n,rem,a[10],i=0;
-  scanf("%d",&n);
-  while(n)
-  {
-    rem=n%2;
-    n=n/2;
-    a[i]=rem;
-    i++;
-  }
-
-  for(int j= i-1;j>=0;j--)
-  {
-    printf("%d",a[j]);
-  }
-
-
-  return 0;
+   int i, j,rows;
+   rows=5;
+   for(i=0;i<rows;i++)
+   {
+     for(j=0;j<2*rows;j++)
+     {
+     if(j<rows-i-1 && j>rows-i+1)
+     {
+       printf("*");
+     }
+     else 
+     {
+       printf(" ");
+     }
+     }
+     printf("\n");
+   }
 }
