201f0031f0a7334186b97288f2cc92161cddef25
Ashish Jha <ashishjhaeee@gmail.com>
2022-12-23 12:06:38 +0000

    Added just the comments to the previous code

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 int main()
 {
   int i,j,k;
-  for(i=1;i<=5;i++)
+  for(i=1;i<=5;i++) // Number of rows 
     {
-      for(j=i;j<5;j++)
+      for(j=i;j<5;j++) // numbers of required spaces 
         {
           printf(" ");
         }
-      for(k=1;k<=i;k++)
+      for(k=1;k<=i;k++) // getting the required number of *
         {
           printf("*");
         }
