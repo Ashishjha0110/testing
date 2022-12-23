a71264811c7706d7e44242f512420af4012657c5
Ashish Jha <ashishjhaeee@gmail.com>
2022-12-23 11:37:10 +0000

    Printing the reverse triangle. ***** **** *** ** *

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main()
 {
-  int i,j,rows; //decalring the variable
-  printf("Please state the number of rows: ");
-  scanf("%d",&rows); // Reading the row data
-  for(i=1;i<= rows;i++)
+  int i,j;
+  for(i=1;i<=5;i++)
     {
-      for(j=1;j<=i;j++)
+      for(j=5;j>=i;j--)
         {
-      printf("* ");
+          printf("*");
         }
-      printf("\n");
-    }
-  return 0;
-
-  }
+    printf("\n");
+ }
+}
