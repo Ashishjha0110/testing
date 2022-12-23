a30ce5884399de6f34d775c350691dbee0a17fb8
Ashish Jha <ashishjhaeee@gmail.com>
2022-12-23 12:04:36 +0000

    printing this Pattern is : ____* ___** __*** _**** *****

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main()
 {
-  int i,j;
+  int i,j,k;
   for(i=1;i<=5;i++)
     {
-      for(j=5;j>=i;j--)
+      for(j=i;j<5;j++)
+        {
+          printf(" ");
+        }
+      for(k=1;k<=i;k++)
         {
           printf("*");
         }
